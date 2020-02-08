6b2770711500aa6a2a985eeb433159913ce46d85
Nikilokser <Nikilokser@mail.ru>
2020-02-08 13:54:03 +0700

    8 sec

--- sketch_jan11a/func.h
+++ sketch_jan11a/func.h
@@ -2,21 +2,21 @@
 #include <Octoliner.h>
 #include <Servo.h>
 #include <SharpIR.h>
+#include "GyverPID.h"
 
 Octoliner octoliner(42);
 Octoliner octoliner1(45);
 Servo servo;
 SharpIR sharp(A6, 1080);
+GyverPID r(0.1, 0.05, 0.01, 20);
 
-int w[] = { -1, -2, -3, -4, -5, -6, -7, -8};
-int w1[] = {8, 7, 6, 5, 4, 3, 2, 1};
+int w1[] = {6, 5, 5, 4, 3, 3, 2, 2}; int w[] = { -2, -2, -3, -3, -4, -5, -5, -6};
 
 int motor[3] = {3, 24, 25};
 int old = 0;
 int encoder = 0;
 int s = 0;
 int deg = 90;
-int default_speed = 80;
 
 unsigned long timing;
 bool left_on = 0;
@@ -35,27 +35,27 @@ int octo;
 
 
 void encoder_plus() {
-    encoder++;
+  encoder++;
 }
 
 float readline() {
-    int sum = 0;
-    int sumw = 0;
-    // считываем значение с датчиков линии
-    for (int i = 0; i < 8; i++) {
-        // выводим значения в Serial-порт
-        sumw += octoliner.analogRead(i) * w[i];
-        sum += octoliner.analogRead(i);
-    }
-    for (int i = 0; i < 8; i++) {
-        // выводим значения в Serial-порт
-        sumw += octoliner1.analogRead(i) * w1[i];
-        sum += octoliner1.analogRead(i);
-    }
-    float res = float(sumw) / sum;
-    res = map(res * 100, -500, 500, -100, 100);
-    res = constrain(res, -100, 100);
-    return res;
+  int sum = 0;
+  int sumw = 0;
+  // считываем значение с датчиков линии
+  for (int i = 0; i < 8; i++) {
+    // выводим значения в Serial-порт
+    sumw += octoliner.analogRead(i) * w[i];
+    sum += octoliner.analogRead(i);
+  }
+  for (int i = 0; i < 8; i++) {
+    // выводим значения в Serial-порт
+    sumw += octoliner1.analogRead(i) * w1[i];
+    sum += octoliner1.analogRead(i);
+  }
+  float res = float(sumw) / sum;
+  res = map(res * 100, -500, 500, -100, 100);
+  res = constrain(res, -100, 100);
+  return res;
 }
 
 float digital_sensors() {
@@ -75,82 +75,82 @@ float digital_sensors() {
   }
   if (sum == 0) sum = 1;
   int res = sumw / sum;
-  res = map(res, -8, 8, -100, 100);
+  //Serial.println(res);
+  res = map(res, -6, 6, -100, 100);
   return res;
 }
 
-float pid(float p, float i, float d, bool digit) {
-    float in;
-    if (digit == 1) in = digital_sensors();
-    else in = readline();
-    float out = 0;
-    float integral;
-    float err = 0 + in;
-    float change_in = old - in;
-    integral += err * i;
-    out += err * p + change_in * d + integral;
-    old = in;
-    out = map(out, -100, 100, 120, 60);
-    out = constrain(out, 60, 120);
-    return out;
-}
-
-//void lights(int s, int d) {
-//  if (s < 100) {
-//    digitalWrite(stopp, 1);
-//  }
-//  else {
-//    digitalWrite(stopp, 0);
-//    if (d > 105) {
-//      digitalWrite(left, left_on);
-//    }
-//    else if (d < 75) {
-//      digitalWrite(right, right_on);
-//    }
-//    else {
-//      digitalWrite(right, 0);
-//      digitalWrite(left, 0);
-//    }
-//  }
+//float pid(float p, float i, float d, bool digit) {
+//  float in;
+//  float sum;
+//  if (digit == 1) in = digital_sensors();
+//  else in = readline();
+//  float out = 0;
+//  sum += in;
+//  out = in * p + (in - old) * d + sum * i;
+//  old = in;
+//  out = map(out, -100, 100, 120, 60);
+//  return out;
 //}
 
+float gyver() {
+  r.input = digital_sensors();
+  r.getResultTimer();
+  return r.output;
+}
+
+void lights(int d) {
+  if (d > 105) {
+    digitalWrite(left, left_on);
+    digitalWrite(right, 0);
+  }
+  else if (d < 75) {
+    digitalWrite(right, right_on);
+    digitalWrite(left, 0);
+  }
+  else {
+    digitalWrite(right, 0);
+    digitalWrite(left, 0);
+  }
+}
+
 void stopsig(int s) {
-    if (s == 0 || (s < 30 && f == 1)) digitalWrite(stopp, 1);
-    else digitalWrite(stopp, 0);
+  if (s == 0 || (s < 30 && f == 1)) digitalWrite(stopp, 1);
+  else digitalWrite(stopp, 0);
 }
 
 void delay_mil() {
-    if (millis() - timing > 250) {
-        left_on = !left_on;
-        right_on = !right_on;
-        timing = millis();
-    }
+  if (millis() - timing > 250) {
+    left_on = !left_on;
+    right_on = !right_on;
+    timing = millis();
+  }
 }
 
 void servo_pos(int degres) {
-    degres = constrain(degres, 60, 120);
-    servo.write(degres);
+  degres = map(degres, -100, 100, 60, 120);
+  degres = constrain(degres, 60, 120);
+  lights(degres);
+  servo.write(degres);
 }
 
 int IRread() {
-    int in = -1;
-    while (Serial1.available()) {
-        in = Serial1.read();
-    }
-    return in;
+  int in = -1;
+  while (Serial1.available()) {
+    in = Serial1.read();
+  }
+  return in;
 }
 
 void waitGreen() {
-    int ir = IRread();
-    while (ir != 2) {
-        do {
-            ir = IRread();
-        } while (ir == -1);
-        Serial.println(ir);
-    }
+  int ir = IRread();
+  while (ir != 2) {
+    do {
+      ir = IRread();
+    } while (ir == -1);
+    //Serial.println(ir);
+  }
 }
-
-
 
 void set() {
   pinMode(stopp, OUTPUT);
@@ -160,66 +160,73 @@ void set() {
   Serial.begin(9600);
   Serial1.begin(115200);
   Wire.begin();
-  
+
   octoliner.begin();
   Serial.println("octoliner ok");
   octoliner1.begin();
   Serial.println("octoliner2 ok");
-  
-  octoliner.setSensitivity(200);
-  octoliner1.setSensitivity(200);
-  
+
+  octoliner.setSensitivity(220);
+  octoliner1.setSensitivity(220);
+
   octoliner.setBrightness(255);
   octoliner1.setBrightness(255);
-  
+
   servo.attach(14);
   Serial.println("servo ok");
-  
+
+  r.setDirection(NORMAL);
+  r.setLimits(-100, 100);
+  r.setpoint = 0;
+
   attachInterrupt(enc, encoder_plus, RISING);
 }
 
 void go(int pwm, bool f) {
-    stopsig(pwm);
-    if (pwm == 0) {
-        digitalWrite(motor[1], 0);
-        digitalWrite(motor[2], 0);
+  stopsig(pwm);
+  if (pwm == 0) {
+    digitalWrite(motor[1], 0);
+    digitalWrite(motor[2], 0);
+  }
+  else {
+    analogWrite(motor[0], pwm);
+
+    if (f == 0) {
+      digitalWrite(motor[1], 0);
+      digitalWrite(motor[2], 1);
     }
     else {
-        analogWrite(motor[0], pwm);
-        
-        if (f == 0) {
-            digitalWrite(motor[1], 0);
-            digitalWrite(motor[2], 1);
-        }
-        else {
-            digitalWrite(motor[1], 1);
-            digitalWrite(motor[2], 0);
-        }
+      digitalWrite(motor[1], 1);
+      digitalWrite(motor[2], 0);
     }
+  }
 }
 
-void jopa() {
-    int dist = sharp.distance();
-    int trace = map(default_speed, 50, 255, 30, 100);
-    if (dist <= trace && st == 0) {
-        digitalWrite(stopp, 1);
-        if (default_speed < 100) {
+int jopa(int scorost) {
+  int dist = sharp.distance();
+  int trace = map(scorost, 50, 255, 30, 85);
+  if (scorost != 0) {
+    if (dist >= trace + 20) {
+      st = 0;
+    }
+    else if (dist < trace) {
+      if (st == 0) {
+        if (scorost < 100) {
           ss = 0;
           ts = 0;
         }
         else {
-          ts = default_speed * 2;
+          ts = scorost * 2;
           ss = 250;
         }
         go(ss, 0);
         delay(ts);
-        f = 1;
-        s = 0;
         st = 1;
+      }
+      else {
+        scorost = 0;
+      }
     }
-    else if (dist >= (trace+5)){
-      s = default_speed;
-      f = 1;
-      st = 0;
-    }
+  }
+  return scorost;
 }
--- sketch_jan11a/sketch_jan11a.ino
+++ sketch_jan11a/sketch_jan11a.ino
@@ -2,83 +2,58 @@
 
 void setup() {
   set();
+  delay(1000);
 }
 
 void loop() {
-  float p = 0.8;
-  float i = 0;
-  float d = 2;
-  s = 80;
-  if (encoder > 5) {
-    p = 0.1;
-//    d = 0.2;
-    s = 200;
+  r.Kp = 1;
+  r.Ki = 0;
+  r.Kd = 5;
+  s = 120;
+  if (encoder > 4) { // первая прямая
+    r.Kp = 0.2;
+    r.Kd = 2;
+    s = 220;
   }
-  if (encoder > 30) {
+  if (encoder > 15){
+    s = 200; 
+  }
+  if (encoder > 48) {
     s = 0;
   }
-//  if (encoder > 30) {
-//    p = 0.2;
-//    d = 0.1;
-//    s = 0;
-//  }
-//  if (encoder > 35) {
-//    p = 1.5;
-//    d = 1.5;
-//    s = 50;
-//  }
-//  if (encoder > 43) {
-//    p = 0.8;
-//    i = 0.4;
-//  }
-//  if (encoder > 75) {
-//    s = 80;
-//  }
-//  if (encoder > 80) {
-//    p = 10;
-//    d = 50;
-//  }
-//  if (encoder > 110) {
-//    s = 120;
-//    p = 3;
-//    d = 10;
-//  }
-//  if (encoder > 130) {
-//    s = 90;
-//  }
-//  if (encoder > 135) {
-//    p = 8;
-//    d = 30;
-//  }
-//  if (encoder > 195) {
-//    s = 120;
-//    p = 3;
-//    d = 10;
-//  }
-//  if (encoder > 200) {
-//    s = 90;
-//  }
-//  if (encoder > 205) {
-//    p = 8;
-//    d = 30;
-//  }
-  deg = pid(p, i, d);
-  go(s, f);
-  servo_pos(deg);
-//  lights(s, deg);
-//  delay_mil();
-//  ir = IRread();
-//  if (ir < 7 && ir != -1 && ir != 0) {
-//    Serial.println(ir);
-//    if (ir == 2 || ir == 3) {
-//      s = 50;
-//    }
-//    else {
-//      s = 0;
-//    }
-//  }
-//  go(s, 90);
-  opa();
-  Serial.println(encoder);
-  delay(1);
+  if (encoder > 57) { // первый поворот
+    r.Kp = 1.5;
+    r.Ki = 1.7;
+    r.Kd = 9;
+    s = 150;
+  }
+  if (encoder > 93) { // вторая прямая
+    r.integral = 0;
+    r.Kp = 0.2;
+    r.Ki = 0;
+    r.Kd = 2;
+    s = 170;
+  }
+  if (encoder > 123) {
+    s = 0;
+  }
+  if (encoder > 125) { // второй поворот
+    r.Kp = 1.5;
+    r.Ki = 1.7;
+    r.Kd = 9;
+    s = 120;
+  }
+  if (encoder > 180) { // финишная прямая
+    s = 200;
+    r.Kp = 0.2;
+    r.Ki = 0;
+    r.Kd = 2;
+  }
+  if (encoder > 225) {
+    s = 0;
+  }
+  int j = jopa(s);
+  go(j, f);
+  servo_pos(gyver());
+  delay_mil();
 }
